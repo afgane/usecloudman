6bcab95eff77bc3d27e6125519f164f35895b902
Enis Afgan <afgane@gmail.com>
2012-06-17 22:14:34 +1000

    Update TinyMCE to version 3.5.2

--- usecloudman/static/js/tiny_mce/textareas.js
+++ usecloudman/static/js/tiny_mce/textareas.js
@@ -5,11 +5,13 @@ tinyMCE.init({
     
     theme_advanced_buttons1 : "formatselect,fontsizeselect,forecolor,|,bold,italic,strikethrough,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,|,link,unlink,|,spellchecker,|,image", 
     theme_advanced_buttons2 : "code,paste,pastetext,pasteword,removeformat,|,backcolor,|,underline,justifyfull,sup,|,outdent,indent,|,hr,anchor,charmap,|,media,|,search,replace,|,fullscreen,|,undo,redo", 
-    theme_advanced_buttons3 : "", 
-    theme_advanced_toolbar_location : "top", 
+    theme_advanced_buttons3 : "",
+    theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
-    theme_advanced_resizing : true, 
+    theme_advanced_resizing : true,
     
-    height : "450", 
-    width: "680"
+    height : "450",
+    width: "680",
+    
+    extended_valid_elements: ",iframe[src|title|width|height|allowfullscreen|frameborder],",
 });
