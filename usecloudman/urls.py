4d4ee27f8bbf4622268932fa5406f2e739c24f29
Enis Afgan <afgane@gmail.com>
2012-06-18 08:48:45 +1000

    Include the current 'enis' portion of site (also as static pages)

--- usecloudman/urls.py
+++ usecloudman/urls.py
@@ -6,10 +6,19 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # url(r'^$', 'usecloudman.views.home', name='home'),
+    # Point to the 'old' static templates
     url(r'^$', direct_to_template, {'template': 'index.html'}, "home"),
     url(r'^publications$', direct_to_template, {'template': 'publications.html'}, "publications"),
     url(r'^screencasts$', direct_to_template, {'template': 'screencasts.html'}, "screencasts"),
-    # url(r'^usecloudman/', include('usecloudman.foo.urls')),
+    # Point to 'enis' part of the page (also reuse the old static pages)
+    url(r'^enis/$', direct_to_template, {'template': 'enis/index.html'}, "enis_home"),
+    url(r'^enis/index.html$', direct_to_template, {'template': 'enis/index.html'}, "enis_home"),
+    url(r'^enis/publications/$', direct_to_template,
+        {'template': 'enis/publications.html'}, "enis_publications"),
+    url(r'^enis/projects/$', direct_to_template,
+        {'template': 'enis/projects.html'}, "enis_projects"),
+    url(r'^enis/contact/$', direct_to_template,
+        {'template': 'enis/contact.html'}, "enis_contact"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^blog/', include('blog.urls')),
